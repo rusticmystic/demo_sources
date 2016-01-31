c81985a6471fd203d93a74f09530e369449dc92e
rusticmystic <mystic4mistie@gmail.com>
2016-01-31 23:20:24 +0530

    new with tree
    Signed-off-by: rusticmystic <mystic4mistie@gmail.com>

--- c/demo_ds.c
+++ c/demo_ds.c
@@ -25,6 +25,8 @@ int    sortList(Node * List);
 Node *   reverseList(Node  * List);
 Node * getLastNode(Node *List);
 Node * getFirstNode(Node *List);
+int    FreeList(Node * List);
+
 
 int showContent(Node *List) {
     Node *Iter = List;
@@ -117,6 +119,7 @@ int sortList(Node * List) {
 
 Node * getLastNode(Node *List) {
     Node * Iter = List;
+    if(!Iter) return;
     while(Iter->next!=NULL)
     {
 
@@ -130,15 +133,84 @@ Node * getFirstNode(Node *List) {
 
 }
 
+int    FreeList(Node * List) {
+    Node * temp = List;
+    Node *Iter = List;
+
+    while(Iter!=NULL) {
+        temp = Iter;
+        Iter = Iter->next;
+        printf(" Free %d\n",temp->key);
+        free(temp);
+    }
+    printf("List %p",List);
+}
+
 /** Binary Tree****/
 
-int addElement(Tree * root, int key);
+//	int addElement(Tree * root, int key);
 int removeElement(Tree * root, int key);
 int Inorder(Tree *root);
 int Preorder(Tree *root);
 int Mirror(Tree *root);
 int showTreeContent(Tree *root);
 
+Tree * addElement(Tree * root, int key)
+{
+    Tree *temp;
+
+    if(!root) {
+        // This is the root node
+        printf("Adding  %d \n", key);
+        temp = (Tree *)malloc(sizeof(Tree));
+        temp->key = key;
+        temp->right=NULL;
+        temp->left=NULL;
+        //root = temp;
+        return temp;
+
+    }
+    else {
+        //Find the location
+
+        if(root->key < key) {
+            printf("Comapring %d and %d Add %d to right\n",root->key,key,key);
+
+            if(root->right) {
+                addElement (root->right,key);
+            }
+            else {
+                root->right = addElement (root->right,key);
+            }
+
+        } else {
+            printf("Comapring %d and %d . Add %d to left\n",root->key,key, key);
+            if(root->left) {
+                addElement (root->left,key);
+            }
+            else {
+                root->left = addElement (root->left,key);
+            }
+        }
+    }
+}
+
+int Inorder(Tree *root) {
+    if(root) {
+        Inorder(root->left);
+        printf(" %d ",root->key);
+        Inorder(root->right);
+    }
+}
+
+int Preorder(Tree *root) {
+    if(root) {
+        printf(" %d ",root->key);
+        Preorder(root->left);
+        Preorder(root->right);
+
+    }
+}
 
 /***Heap******/
 
@@ -147,31 +219,52 @@ typedef Node List;
 int main(int arg, char * argv[]) {
 
     // Create a List
-    List *MyList;
+#if 1
+    List *MyList=NULL;
     MyList=addNode(MyList,10);
     //    printf("List Demo %p\n",MyList->next);
-    
+
     // Add Data to the Nodes
     addNode(MyList,8);
     addNode(MyList,4);
     addNode(MyList,7);
     addNode(MyList,9);
     addNode(MyList,12);
-    
-    //    printf("List Demo %p\n",MyList);
 
-    // Display 
-    showContent(MyList);
-    MyList=reverseList(MyList);
-    
     //    printf("List Demo %p\n",MyList);
-
-    // Delete Nodes
-    delNode(MyList, 9);
-    delNode(MyList, 4);
 
     // Display
     showContent(MyList);
+    MyList=reverseList(MyList);
 
+    //    printf("List Demo %p\n",MyList);
+
+    // Delete Nodes
+    //delNode(MyList, 9);
+    //delNode(MyList, 4);
+
+    // Display
+    showContent(MyList);
+#endif
+
+    FreeList(MyList);
+    printf("\n List Freed \n");
+    showContent(MyList);
+    // Tree
+    Tree *MyTree = NULL;
+
+#if 1
+    // addElement(&MyTree,10);
+    MyTree = addElement(MyTree,110);
+    addElement(MyTree,10);
+    addElement(MyTree,40);
+    addElement(MyTree,30);
+#endif
+
+    printf("InOrder \n");
+    Inorder(MyTree);
+
+    printf("\n PreOrder \n");
+    Preorder(MyTree);
     return 0;
 }
